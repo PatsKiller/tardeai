42847cb963448ceae11beb0e85c61f073f62d689
PatsKiller <john@bestratemotors.com>
2026-07-23 16:45:22 -0400

    feat(reentry): mount operator guide and analyst evidence

--- apps/command-center-v3/src/pages/ReEntryPageV4.tsx
+++ apps/command-center-v3/src/pages/ReEntryPageV4.tsx
@@ -1,7 +1,9 @@
 import { useSearchParams } from 'react-router-dom'
 import AuthoritativeExitUniverse from '../components/reentry/AuthoritativeExitUniverse'
+import ReEntryAnalystEvidence from '../components/reentry/ReEntryAnalystEvidence'
 import ReEntryAnalystLookthroughBoard from '../components/reentry/ReEntryAnalystLookthroughBoard'
 import ReEntryExitDetailLedger from '../components/reentry/ReEntryExitDetailLedger'
+import ReEntryHelpGuide from '../components/reentry/ReEntryHelpGuide'
 import ReEntryResistanceBoard from '../components/reentry/ReEntryResistanceBoard'
 import ReEntryRotationWorkspace from '../components/reentry/ReEntryRotationWorkspace'
 
@@ -9,8 +11,10 @@ export default function ReEntryPageV4() {
   const [params] = useSearchParams()
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
-      <ReEntryExitDetailLedger />
+      <ReEntryHelpGuide />
       <AuthoritativeExitUniverse />
+      <ReEntryExitDetailLedger />
+      <ReEntryAnalystEvidence />
       <ReEntryResistanceBoard />
       <ReEntryAnalystLookthroughBoard />
       <ReEntryRotationWorkspace mode="full" initialSymbol={params.get('symbol') ?? ''} />
